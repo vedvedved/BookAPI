9654c2179158bf5ec16ec33ab368eb81ebc0a413
vedvedved <vedikajamdar@gmail.com>
2021-11-18 14:47:01 +0530

    new connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+
 
 //Importing schemas
 const Book = require('./schema/book');
